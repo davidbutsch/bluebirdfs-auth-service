82a94d95493183a546973255a03267eca4848cca
David <102764748+davidbzyae@users.noreply.github.com>
2024-02-28 15:18:14 -0600

    feat(UserService): add findById and authenticateUser methods

--- src/modules/user/services/IUserService.ts
+++ src/modules/user/services/IUserService.ts
@@ -1,7 +1,10 @@
 import { User, UserDTO } from "@/modules/user";
 
+import { CredentialsDTO } from "@/modules/session";
+
 export interface IUserService {
   findById(id: string): Promise<UserDTO | null>;
+  authenticateUser(credentials: CredentialsDTO): Promise<UserDTO>;
   create(user: Partial<User>): Promise<UserDTO>;
   updateName(id: string, update: string): Promise<UserDTO | null>;
   delete(id: string): Promise<void>;
--- src/modules/user/services/UserService.ts
+++ src/modules/user/services/UserService.ts
@@ -1,13 +1,46 @@
-import { User, UserDTO } from "@/modules/user";
+import { Error, Types } from "mongoose";
+import { IUserRepository, User, UserDTO, IUserService } from "@/modules/user";
 
-import { IUserService } from "./IUserService";
-import { Types } from "mongoose";
-import { injectable } from "tsyringe";
+import { CredentialsDTO } from "@/modules/session";
+import { inject, injectable } from "tsyringe";
+import { AppError } from "@/errors";
+import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcrypt";
 
 @injectable()
 export class UserService implements IUserService {
+  constructor(
+    @inject("UserRepository") private userRepository: IUserRepository
+  ) {}
+
   async findById(id: string): Promise<UserDTO | null> {
+    const user = await this.userRepository.findById(id);
+
+    if (user) return UserDTO.toDTO(user);
+
     return null;
+  }
+  async authenticateUser(credentials: CredentialsDTO): Promise<UserDTO> {
+    const user = await this.userRepository.findByEmail(credentials.email);
+
+    if (!user)
+      throw new AppError(StatusCodes.UNAUTHORIZED, "Invalid credentials");
+
+    if (!user.security.password)
+      throw new AppError(
+        StatusCodes.UNAUTHORIZED,
+        "Email registered with social account"
+      );
+
+    const passwordsMatch = await bcrypt.compare(
+      credentials.password,
+      user.security.password
+    );
+
+    if (!passwordsMatch)
+      throw new AppError(StatusCodes.UNAUTHORIZED, "Invalid credentials");
+
+    return UserDTO.toDTO(user);
   }
   create(user: Partial<User>): Promise<UserDTO> {
     throw new Error("Method not implemented.");
