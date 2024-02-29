6f42e3c559ef6b6c89c0ba9e51d025e28ebc363a
David <102764748+davidbzyae@users.noreply.github.com>
2024-02-29 17:17:25 -0600

    feat(UserController): add get me controller endpoint

--- src/modules/user/controllers/UserController.ts
+++ src/modules/user/controllers/UserController.ts
@@ -1,18 +1,30 @@
-import { Body, Get, HttpCode, JsonController, Post } from "routing-controllers";
+import {
+  Body,
+  Get,
+  HttpCode,
+  JsonController,
+  Post,
+  Res,
+  UseBefore,
+} from "routing-controllers";
+
+import { Response } from "express";
 
 import { CreateUserDTO, IUserService } from "@/modules/user";
 import { inject, injectable } from "tsyringe";
-import { AppError } from "@/errors";
-import { StatusCodes } from "http-status-codes";
+import { AttachSession } from "@/middlewares";
 
 @injectable()
 @JsonController("/users")
 export class UserController {
   constructor(@inject("UserService") private userService: IUserService) {}
 
+  @UseBefore(AttachSession)
   @Get("/me")
-  getMe() {
-    throw new AppError(StatusCodes.NOT_IMPLEMENTED, "Route not implemented.");
+  getMe(@Res() res: Response) {
+    const session = res.locals.session;
+
+    return this.userService.findById(session.userId);
   }
 
   @HttpCode(201)
